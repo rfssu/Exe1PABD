8417a44bc07aa3afac69292ec78e1cf66571e2fa
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2022-03-26 15:04:30 +0700

    Add project files.

---
+++ Exe1PABD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Exe1PABD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
